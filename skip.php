402eec17e3db40e9bba46cc173ed3f6250a0ab54
William Venner <william@venner.io>
2017-08-26 16:59:00 +0100

    getallheaders error handling in skip.php

--- skip.php
+++ skip.php
@@ -1,5 +1,19 @@
 <?php
-    
+	
+	if (!function_exists("getallheaders")) {
+		
+		// getallheaders is required to get the Authorization header as it does not appear
+		// in $_SERVER or anywhere else. Therefore, try updating all of your software -
+		// web server, PHP, etc. and try again.
+		
+		// This works absolutely fine on Apache and PHP 7.
+		
+		http_response_code("501 Not Implemented");
+		error_log("https://github.com/WilliamVenner/LaMetric-Spotify/issues/2#issuecomment-325140347");
+		return;
+		
+	}
+
 	$headers = getallheaders();
 	
 	if (!isset($headers["Authorization"])) {
